8f63f25591b25ab4d5a6ec632333fdf98907cffd
Santiago Pérez Ruiz <145030459+santiag0perez@users.noreply.github.com>
2023-12-24 20:39:10 -0500

    Update settings.py

--- personal/settings.py
+++ personal/settings.py
@@ -116,13 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 # Default primary key field type
